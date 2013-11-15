bc882010590e35aff5ad2f48dec2c900ab97fe26
Vincent <vince78718@gmail.com>
2013-11-15 22:25:01 +0800

    Initial Commit

---
+++ uiviewbackground/KMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KMAppDelegate.h
+//  uiviewbackground
+//
+//  Created by Vincent on 2013/11/15.
+//  Copyright (c) 2013年 Kumaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ uiviewbackground/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  uiviewbackground
+//
+//  Created by Vincent on 2013/11/15.
+//  Copyright (c) 2013年 Kumaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KMAppDelegate class]));
+    }
+}
