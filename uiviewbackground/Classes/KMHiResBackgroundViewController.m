b695e5064eab57dfce0a1a48613cf38dc48f5774
Vincent <vince78718@gmail.com>
2013-11-16 09:15:03 +0800

    Complete background setting with scale control.

--- uiviewbackground/Classes/KMHiResBackgroundViewController.m
+++ uiviewbackground/Classes/KMHiResBackgroundViewController.m
@@ -37,6 +37,24 @@
     KMAppDelegate *appDelegate     = (KMAppDelegate *)[[UIApplication sharedApplication] delegate];
     UIImage       *backgroundImage = appDelegate.backgroundImage;
     
+    UIView        *demoView        = [[UIView alloc] initWithFrame:CGRectMake(6, 20 + 6, 308, 140)];
+    
+    UIImage  *buttonBackgroundImage = [backgroundImage resizableImageWithCapInsets:UIEdgeInsetsMake(3, 3, 3, 3)];
+    
+    // !!!: 修改 function ，加入 scale 參數 (0.0f)，設定第二個參數 opaque 為 NO
+    UIGraphicsBeginImageContextWithOptions(demoView.frame.size, NO, 0.0f);
+    
+    [buttonBackgroundImage drawInRect:demoView.bounds];
+    UIImage *newButtonBackgroundImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    // !!!: 指定背景
+    demoView.backgroundColor       = [UIColor colorWithPatternImage:newButtonBackgroundImage];
+    
+    self.view.backgroundColor      = [UIColor lightGrayColor];
+    
+    [self.view addSubview:demoView];
 }
 
 - (void)didReceiveMemoryWarning
